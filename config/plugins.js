5465b5c88ab895d127e54d5c55ba49dad97e5ac3
tigorthemusichead <arsenij.ermolaev2003@gmail.com>
2023-05-09 15:28:09 +0400

    projects

--- config/plugins.js
+++ config/plugins.js
@@ -4,4 +4,20 @@ module.exports = ({ env }) => ({
       defaultDepth: 3, // Default is 5
     }
   },
+  email: {
+    provider: 'nodemailer',
+    providerOptions: {
+      host: env('SMTP_HOST', 'smtp.example.com'),
+      port: env('SMTP_PORT', 587),
+      auth: {
+        user: env('SMTP_USERNAME'),
+        pass: env('SMTP_PASSWORD'),
+      },
+      // ... any custom nodemailer options
+    },
+    settings: {
+      defaultFrom: env('SMTP_USERNAME'),
+      defaultReplyTo: env('SMTP_USERNAME'),
+    },
+  },
 });
